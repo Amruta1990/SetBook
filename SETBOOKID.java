e126d80bf207a03f89659b3e1c9adcdc26acf552
Amruta1990 <118837878+Amruta1990@users.noreply.github.com>
2022-12-20 12:55:41 +0530

    Add files via upload
    task done

---
+++ SETBOOKID.java
@@ -0,0 +1,46 @@
+package setBook;
+public class SETBOOKID {
+	private int id;
+	private String name;
+	private double price;
+	private String title;
+	private String author;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	@Override
+	public String toString() {
+		return "SETBOOKID [id=" + id + ", name=" + name + ", price=" + price + ", title=" + title + ", author=" + author
+				+ "]";
+	}
+}
+
+	
+	
